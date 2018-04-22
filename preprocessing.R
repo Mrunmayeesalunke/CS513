b8507577c893c526d3c1b2b46abffb0cbfccce72
wzamlynny <wzamlynny@gmail.com>
2018-04-22 15:01:55 -0400

    change y to landuse

--- preprocessing.R
+++ preprocessing.R
@@ -8,9 +8,9 @@
 #  Name 3      : Weronika Zamlynny (10397182)
 #  Date        : 05/02/2018
 #  Comments    : 
-
-
+#################################################
 rm(list=ls())
+#################################################
 
 #Load Data
 ?read.csv
@@ -21,18 +21,19 @@ df
 #Summarizing each column
 summary(df)
 
+#################################################
 #Modifying columns
 #Replacing NA with 0 in all of the areas excluding LotArea and BldgArea
 #column_na<-is.na(df[,"ResArea"])
 #df[column_na,"ResArea"]<-0
 #df
 
-#Make new column for Majority_Res
-df["Majority_Res"]<-df[,"ResArea"]>(df[,"ComArea"]+df[,"OfficeArea"]+df[,"RetailArea"]+df[,"GarageArea"]+df[,"StrgeArea"]+df[,"FactryArea"])
-df <- subset(df, select=-c(ResArea,ComArea,OfficeArea,RetailArea,GarageArea,StrgeArea,FactryArea))
-#Remove OtherArea
-df <- subset(df, select=-OtherArea)
+# TODO use LandUse insteado Majority Area
+# Make new column Residential if Land Use <= 4
+df["Residential"]<- ifelse(df[,"LandUse"]=='1'|df[,"LandUse"]=='2'|df[,"LandUse"]=='3'|df[,"LandUse"]=='4',TRUE,FALSE)
+df <- subset(df, select=-LandUse)
 
+#################################################
 #Combine Year Alter 1 and 2
 df["YearAlter"]<-ifelse(df[,"YearAlter1"]>df[,"YearAlter2"],df [,"YearAlter1"],df[,"YearAlter2"])
 df <- subset(df, select=-c(YearAlter1,YearAlter2))
@@ -55,7 +56,7 @@ df <- subset(df, select=-LtdHeight)
 df[,"Ext"]<-!is.na(df[,"Ext"])
 df <- subset(df, select=-Ext)
 
-
+#################################################
 # Delete Address - not useful string, most in other
 df <-subset(df, select=-Address)
 # Delete Owner Name, not useful string
@@ -64,12 +65,25 @@ df <- subset(df, select=-OwnerName)
 # Delete X-cord y-cord, not useful
 df <- subset(df, select=-c(XCoord, YCoord))
 
+# Delete Dataset Version Number
+df <- subset(df, select=-Version)
 
+#################################################
 # Delete variables that directly mean Residential
-df <-subset(df, select=-c(Overlay1,Overlay2))
+
+# Areas give away if Residential
+df <- subset(df, select=-c(ResArea,ComArea,OfficeArea,RetailArea,FactryArea,OtherArea))
 # Zone Dist may directly tell us residential / had many NA
 df <- subset(df, select=-c(ZoneDist1,ZoneDist2,ZoneDist3,ZoneDist4))
+# Delete Commercial Overlay
+df <- subset(df, select=-c(Overlay1,Overlay2))
+# Delete Number of Residential Units
+df <- subset(df, select=-UnitsRes)
+# Delete CondoNo, only residential have non 0
+df <- subset(df, select=-CondoNo)
 
+#################################################
+# Dealing with NA
 
 # OwnerType According to specs NA usually means Privately Own 17005
 df[is.na(df[,"OwnerType"]), "OwnerType"] <- "P"
@@ -91,3 +105,10 @@ df <- subset(df, select=-APPDate)
 # Removing other NA's
 df <- na.omit(df)
 
+#################################################
+# Remove duplicate Borough indicators
+df <- subset(df, select=-Borough)
+
+#################################################
+# Print to file
+write.csv(df, "nyc-buildings/BK_processed.csv")
