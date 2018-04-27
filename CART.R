bf395df57c6b875d0f0383fece00e177128d0512
wzamlynny <wzamlynny@gmail.com>
2018-04-27 19:08:44 -0400

    Update CART to use x features

--- CART.R
+++ CART.R
@@ -1,26 +1,17 @@
-#########################################################
-##  Purpose  : Create pretty classification tree
-##  Developer: Project Group       
-##
-#########################################################
-
-#########################################################
-##  Step 0: Clear the environment
-##           
-##
-#########################################################
+#################################################
+#  Company     : Stevens 
+#  Project     : CS 513 Final Project-NYC Buildings Data
+#  Purpose     : CART Tree
+#  Names of the group members:
+#  Name 1      : Mrunmayee Salunke (10429075)
+#  Name 2      : Heli Choksi       (10430926)
+#  Name 3      : Weronika Zamlynny (10397182)
+#  Date        : 05/02/2018
+#  Comments    : 
+#################################################
 rm(list=ls())
-
-
-
-
-
-#########################################################
+#################################################
 ##  Step 1: Load the relavent packages
-##           
-##
-#########################################################
-
 #install.packages("rpart")
 #install.packages("rpart.plot")     # Enhanced tree plots
 #install.packages("rattle")         # Fancy tree plot
@@ -29,58 +20,32 @@ library(rpart)
 library(rpart.plot)  			# Enhanced tree plots
 library(rattle)           # Fancy tree plot
 library(RColorBrewer)     # colors needed for rattle
-
 #########################################################
 ##  Step 2:  Loading the data
-
-rm(list=ls())
 df<- read.csv("C:/Users/mrunm/Desktop/CS513 Project/BK_processed.csv")
 #View(df)
 
-df<-subset(df,select=-X)
-#df<-subset(df,select=-BldgClass) # delete the building class
-#df<-subset(df,select=-Sanborn)
-#df<-subset(df,select=-CT2010)
+# Change y to as factor
 df$Residential<-as.factor(df$Residential)
+
+# Split to training and test dataset
 index<-sort(sample(nrow(df),round(.25*nrow(df))))
 training<-df[-index,]
 test<-df[index,]
 
-
-?rpart()
-
-
-#Grow the tree 
-CART_class<-rpart( Residential~.,data=training)
+# Grow the tree 
+CART_class<-rpart( Residential ~ UnitsTotal+AssessTot+NumFloors+AssessLand+BldgArea+BuiltFAR+StrgeArea+GarageArea+BldgDepth+ExemptTot, data=training)
 CART_class
 
-prp(CART_class) #Successful tree
-
-#?prp
-#Delete successful parameters
-#df<-subset(df,select=-c(UnitsTotal,AssessLand,GarageArea,StrgeArea,NumFloors))
-#training<-subset(training,select=-c(UnitsTotal,AssessLand,GarageArea,StrgeArea,NumFloors))
-#test<-subset(test,select=-c(UnitsTotal,AssessLand,GarageArea,StrgeArea,NumFloors))
-
-###Grow the tree 
-#CART_class<-rpart( Residential~.,data=training)
-#CART_class
-#prp(CART_class) #Successful tree
-
-
-
+# Plot tree
+prp(CART_class)
 fancyRpartPlot(CART_class,cex=1)
 
-
-#prediction
+# Prediction
 Prediction <- predict(CART_class, test,type='class')
 table(actual=test$Residential,Prediction)
 
-
-
-#Finding the Error Rate
+# Finding the Error Rate
 wrong<- (test$Residential!=Prediction )
 error_rate<-sum(wrong)/length(wrong)
 error_rate 
-
-
